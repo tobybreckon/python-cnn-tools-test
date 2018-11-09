d091f2ce1a30ea81d074b1e1917e06d4a7407010
tobybreckon <toby.breckon@cranfield.ac.uk>
2018-11-09 16:50:56 +0000

    initial version

---
+++ mxnet-test.py
@@ -0,0 +1,45 @@
+#####################################################################
+
+# Example : test if mxnet environment is working
+
+# Author : Bruna Maciel-Pearson
+
+# Copyright (c) 2018 Bruna Maciel-Pearson / Toby Breckon
+# Dept. Computer Science, Durham University, UK
+
+# License : MIT
+
+#####################################################################
+
+import mxnet
+
+import numpy as np
+import sys
+import matplotlib
+import os
+
+#####################################################################
+
+print("We are using MXNet: " + mxnet.__version__);
+print();
+# list GPUs available
+gpus = mxnet.test_utils.list_gpus()
+if len(gpus) >=1:
+	print("MXNET has "+str(len(gpus))+" GPUs available.")
+	for i in gpus:
+		print("The GPUs available are: ")
+		print("- GPU number:",i)
+else:
+	print("MXNet says - sorry no GPU found")
+
+print();
+
+#####################################################################
+
+# check other stuff
+
+print("We are using numpy: " + np.__version__);
+print("We are using matplotlib: " + matplotlib.__version__);
+print(".. and this is in Python: " + sys.version)
+
+#####################################################################
